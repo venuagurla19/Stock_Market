11dcc876cbee0b9bae549a78c6930f6beae84c1f
venu <agurlavenu1@gmail.com>
2025-04-04 08:22:45 +0530

    venu

---
+++ EKS/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-west-2"
+    }
+
+resource "aws_s3_bucket" "venu_bucket" {
+    bucket = "terrafrom_test-bucket"
+
+lifecycle {
+    prevent_destroy = false
+    }
+}
+
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+    name           = "GameScores"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "UserId"
+    
+
+    attribute {
+    name = "UserId"
+    type = "S"
+    }
